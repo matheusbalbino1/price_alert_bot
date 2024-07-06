9dff0ac31eb77a7b8a31836b4f3789fd872a7673
MatheusBalbino1 <matheus.balbino@timeenergy.com.br>
2024-07-05 23:01:25 -0300

    improve filter

--- script.py
+++ script.py
@@ -9,12 +9,6 @@ load_dotenv()
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 TOKEN_MERCADO_LIVRE = os.environ.get('TOKEN_MERCADO_LIVRE')
 CHAT_ID = os.environ.get('CHAT_ID')
-
-print('#'*200)
-print(TELEGRAM_BOT_TOKEN)
-print(TOKEN_MERCADO_LIVRE)
-print(CHAT_ID)
-
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 PRODUCTS_TO_SEARCH = [
@@ -59,10 +53,15 @@ async def run():
     for item in response.json()["results"]:
       title = item["title"]
       price = item["price"]
-      link = item ["permalink"]
-      contains_substring = any(word_to_avoid.lower() in title.lower() for word_to_avoid in product["avoid"])
+      link = item["permalink"]
       
-      if not contains_substring and item["accepts_mercadopago"] and item["currency_id"] == "BRL":
+      names_product = [str(attribute["value_name"]) for attribute in item["attributes"]]
+
+      second_filter_product = any(product["product"].lower() in name.lower()  for name in names_product)
+
+      contains_substring_to_avoid = any(word_to_avoid.lower() in title.lower() for word_to_avoid in product["avoid"])
+      
+      if second_filter_product and not contains_substring_to_avoid and item["accepts_mercadopago"] and item["currency_id"] == "BRL":
         message = f"""
         <b>{title}</b> for <b>{price}</b>, <a href="{link}">Click to redirect</a>
         """
